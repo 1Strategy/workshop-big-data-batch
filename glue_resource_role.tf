e6982d4b67eab255e11f34fd92381f915ce7dcae
Alex <agrvs@users.noreply.github.com>
2017-09-19 10:54:51 -0600

    initial commit of glue_resource_role.tf
    The Glue service will assume this role.

---
+++ glue_resource_role.tf
@@ -0,0 +1,44 @@
+## GlueResourceRole
+## The Glue service will assume this role when it runs crawlers, launches jobs, and builds supporting infrastructure.
+#
+#  1. Create the GlueResourceRole.
+#  2. Attach the AWSGlueServiceRole to the GlueResourceRole.
+#  3. Establish trust relationship between GlueResourceRole and the Glue service.
+#  4. Allow S3 actions on the galactic-map-tiles bucket.
+
+
+resource "aws_iam_role" "GlueResourceRole" {
+  name = "GlueResourceRole"
+
+  assume_role_policy = "${data.aws_iam_policy_document.GlueResourceRole_trust_relationship.json}"
+  policy = "${data.aws_iam_policy_document.GlueResourceRole_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "GlueResourceRole_attachment" {
+    role       = "${aws_iam_role.GlueResourceRole.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+data "aws_iam_policy_document" "GlueResourceRole_trust_relationship" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "GlueResourceRole_policy" {
+  statement {
+    sid = "AllowAllS3Actions"
+
+    actions = ["s3:*"]
+
+    resources = [
+      "arn:aws:s3:::galactic-map-tiles",
+      "arn:aws:s3:::galactic-map-tiles/*"
+    ]
+  }
+}

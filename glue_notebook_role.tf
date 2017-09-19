8333531642b377a230a4f143eb75de1e8bf14ee8
Alex <agrvs@users.noreply.github.com>
2017-09-19 10:58:15 -0600

    add comments

--- glue_notebook_role.tf
+++ glue_notebook_role.tf
@@ -1,13 +1,21 @@
-resource "aws_iam_role_policy_attachment" "attachment" {
-    role       = "${aws_iam_role.GlueNotebookRole.name}"
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceNotebookRole"
-}
+## GlueNotebookRole
+## Development endpoints and Zeppelin notebook servers will be launched with this role when created by the Glue service.
+#
+#  1. Create the GlueNotebookRole.
+#  2. Attach the AWSGlueNotebookRole policy to the GlueNotebookRole.
+#  3. Establish a trust relationship between the GlueNotebookRole and the EC2 service.
+#  4. Allow S3 actions on the navigation-notebooks bucket.
 
 resource "aws_iam_role" "GlueNotebookRole" {
   name = "GlueNotebookRole"
 
   assume_role_policy = "${data.aws_iam_policy_document.GlueNotebookRole_trust_relationship.json}"
   policy = "${data.aws_iam_policy_document.GlueNotebookRole_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+    role       = "${aws_iam_role.GlueNotebookRole.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceNotebookRole"
 }
 
 data "aws_iam_policy_document" "GlueNotebookRole_trust_relationship" {
